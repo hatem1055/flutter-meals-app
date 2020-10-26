046fa4f66be6cf6995e9d3afd410e7acbd2147da
hatem1055 <hatemmostafa31@gmail.com>
2020-10-26 23:53:16 +0200

    categories screen

---
+++ lib/screens/categories_screen/categories_main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../cetegries_data.dart';
+import './category_item.dart';
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('DaliMeal'),
+        ),      
+        body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: [
+          ...categories.map((category){
+            return CategoryWidget(category);
+          }).toList()
+        ],
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent:200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+      ),
+    );
+  }
+}
