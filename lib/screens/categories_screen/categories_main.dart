4873f376ac0e1c9ccb5a52fe1584eafc119bcbd4
hatem1055 <hatemmostafa31@gmail.com>
2020-10-31 01:26:19 +0200

    meals data

--- lib/screens/categories_screen/categories_main.dart
+++ lib/screens/categories_screen/categories_main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../cetegries_data.dart';
+import '../../data.dart';
 import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
--- lib/screens/category_meals/category_meals_main.dart
+++ lib/screens/category_meals/category_meals_main.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data.dart';
 import '../../models/category.dart';
+import '../../data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routName = 'categorie-meals';
+
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = meals.where((meal){
+      return meal.categoryIds.contains(category.id);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
         body: Center(
-      child: Text('Meals'),
+      child: ListView.builder(itemBuilder: (ctx,index){
+        return Text(categoryMeals[index].title);
+      },
+      itemCount: categoryMeals.length,
+      ),
     ));
   }
 }
